fd254c9a555f053a6871adfb17d117c5c07bcfcb
Richard Gottleber <richard@gottleber.net>
2022-04-04 14:01:21 -0500

    initial button

---
+++ src/lib/WalletConnect.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { ethers } from 'ethers';
+	export let web3Props = { provider: null, signer: null, account: null, chainId: null };
+	async function connectWallet() {
+		const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+		await provider.send('eth_requestAccounts', []);
+		const signer = provider.getSigner();
+		const account = await signer.getAddress();
+		const chainId = await signer.getChainId();
+		web3Props = { signer, provider, chainId, account };
+	}
+</script>
+
+{#if !web3Props.account}
+	<button on:click={connectWallet}>Attach Wallet</button>
+{/if}
